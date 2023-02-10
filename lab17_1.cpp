c8c1b7153f25dc5ae876d96e5fca20516bde2899
fluke <adisorn.sjr@gmail.com>
2023-02-10 17:14:33 +0700

    baaa

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,42 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if(i%M==M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (rand()%101)/100.00;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+	double x=0;
+	int j=0;
+	for(int i = 0;i<M*N;i++){
+		x = x + *(a+i);  
+		if(i%M==M-1){
+			b[j] = x;
+			x = 0;
+			j++;
+		}
+		
+		
+	}
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    for(int i = 0;i<N*M;i++){
+        *(b+i%M) = 0;
+    }
+    for(int j=0;j<N*M;j++){
+        *(b+j%M) += *(a+j);
+    }
+}
